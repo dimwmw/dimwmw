89423d3f1174123b47e5af46178cef9232d3559f
dimasw <48420325+dimaz0897@users.noreply.github.com>
2024-09-29 07:09:25 +0700

    Create answer.py

---
+++ answer.py
@@ -0,0 +1,98 @@
+{
+    "2024-09-14": {
+        "choice_1": 4,
+        "choice_2": 8,
+        "choice_3": 15,
+        "choice_4": 16
+    },
+    "2024-09-15": {
+        "choice_1": 8,
+        "choice_2": 4,
+        "choice_3": 10,
+        "choice_4": 1
+    },
+    "2024-09-16": {
+        "choice_1": 3,
+        "choice_2": 10,
+        "choice_3": 1,
+        "choice_4": 9
+    },
+    "2024-09-17": {
+        "choice_1": 10,
+        "choice_2": 4,
+        "choice_3": 1,
+        "choice_4": 16
+    },
+    "2024-09-18": {
+        "choice_1": 8,
+        "choice_2": 3,
+        "choice_3": 7,
+        "choice_4": 4
+    },
+    "2024-09-19": {
+        "choice_1": 9,
+        "choice_2": 4,
+        "choice_3": 10,
+        "choice_4": 2
+    },
+    "2024-09-20": {
+        "choice_1": 3,
+        "choice_2": 5,
+        "choice_3": 1,
+        "choice_4": 2
+    },
+    "2024-09-21": {
+        "choice_1": 13,
+        "choice_2": 2,
+        "choice_3": 11,
+        "choice_4": 10
+    },
+    "2024-09-22": {
+        "choice_1": 6,
+        "choice_2": 7,
+        "choice_3": 5,
+        "choice_4": 4
+    },
+    "2024-09-23": {
+        "choice_1": 4,
+        "choice_2": 3,
+        "choice_3": 2,
+        "choice_4": 1
+    },
+    "2024-09-24": {
+        "choice_1": 5,
+        "choice_2": 7,
+        "choice_3": 8,
+        "choice_4": 9
+    },
+    "2024-09-25": {
+        "choice_1": 3,
+        "choice_2": 4,
+        "choice_3": 7,
+        "choice_4": 6
+    },
+    "2024-09-26": {
+        "choice_1": 13,
+        "choice_2": 14,
+        "choice_3": 11,
+        "choice_4": 12
+    },
+    "2024-09-27": {
+        "choice_1": 10,
+        "choice_2": 4,
+        "choice_3": 5,
+        "choice_4": 1
+    },
+    "2024-09-28": {
+        "choice_1": 3,
+        "choice_2": 5,
+        "choice_3": 1,
+        "choice_4": 7
+    },
+    "2024-09-29": {
+        "choice_1": 3,
+        "choice_2": 5,
+        "choice_3": 1,
+        "choice_4": 7
+    }
+}
